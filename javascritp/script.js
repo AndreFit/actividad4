56d1a2942dc890bcc1d8853148d8bfe5611abcf0
AndreFit <agonzalezr@iberoamericana.edu.co>
2022-03-27 20:00:45 -0500

    BIENVENIDO A LA ACTIVIDAD 4

---
+++ javascritp/script.js
@@ -0,0 +1,70 @@
+$(".definicion").css("color", "blue")
+
+$(".definicion1").click(function(){
+    $(".definicion1").css("font-size","50px")
+})
+
+$(".definicion2").dblclick(function(){
+    $(".definicion2").css("background", "white")
+})
+
+$(".definicion3").click(function(){
+    $(".definicion3").css("font-size", "80px")
+})
+$(".evento3").dblclick(function(){
+    $(".evento3").css("font-size", "20px")
+})
+
+$(".definicion4").click(function(){
+    alert("hola")
+})
+
+$(".definicion5").click(function(){
+    $(".definicion5").hide();
+})
+
+$(".definicion6").mouseenter(function(){
+    $(".definicion6").hide();
+    
+})
+$(".definicion6").mouseleave(function(){
+    $(".definicion6").show();
+    
+})
+
+$(".definicion7").mouseenter(function(){
+    alert("  es la estructura del sistema nervioso central que ocupa mayor espacio en la cavidad craneal, ya que está compuesto de dos hemisferios que a su vez se dividen en 5 lóbulos. Esta región, al igual que las otras partes del encéfalo debe ser cuidado, ya que es el principal encargado de coordinar las funciones del cuerpo.")
+})
+
+$(".definicion8").dblclick(function(){
+    $(".evento8").css("font-size", "70px", )
+})
+$(".definicion8").dblclick(function(){
+    alert("El colágeno es una sustancia clasificada como proteína, que se localiza en varios tejidos del cuerpo humano. Y que tiene múltiples beneficios para la piel, el sistema óseo, articulaciones, vasos sanguíneos, ojos, en algunos órganos y estructuras")
+})
+
+$(".definicion9").click(function(){
+    $(".definicion9").css("color","MediumSpringGreen")
+})
+$(".definicion9").dblclick(function(){
+    $(".definicion9").css("color","black")
+})
+$(".definicion10").mousemove(function(){
+    $(".definicion10").css("color","Teal")
+})
+
+$(".definicion11").mousedown(function(){
+    alert("contenido no encontrado")
+})
+
+
+
+$(".caja1").mouseover(function(){
+    $(".caja1").css("background", "PaleVioletRed")
+})
+$(".caja2").mouseover(function(){
+    $(".caja2").css("background", "Coral")
+})
+$(".caja3").mouseover(function(){
+    $(".caja3").css("background", "khaki")
+})
